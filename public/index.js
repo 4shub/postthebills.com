32961eb2f35b7fa1323ca1433c04b5706a5e7224
Shubham Naik <shubhamnaik@Shubhams-Mac-mini.local>
2024-04-22 20:54:19 -0700

    chore: next

--- public/index.js
+++ public/index.js
@@ -7,15 +7,16 @@ window.onload = function () {
   if (memoryCache) {
     localStorage.removeItem('memoryCache');
     window.location.href = memoryCache;
+
+    return;
   }
 
   const prevData = document.getElementById('prev').dataset.prev;
 
-  if (prevData) {
+  if (prevData !== '_PREV_DATA_') {
     localStorage.setItem('memoryCache', `/${prevData}.html`);
   } else {
     localStorage.setItem('memoryCache' , '/');
   }
-
 
 }
